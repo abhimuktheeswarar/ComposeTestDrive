334758ac1d1dc93ab3eb52aeedfc709a538d4400
Abhi Muktheeswarar <abhi.muktheeswarar@redbus.com>
2022-01-12 16:39:46 +0530

    Add imperative vs declarative code
    Add Starter compose activity.

--- app/src/main/java/com/msabhi/composedrive/MainActivity.kt
+++ app/src/main/java/com/msabhi/composedrive/MainActivity.kt
@@ -6,11 +6,14 @@ import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.msabhi.composedrive.declarative.DeclarativeCounterActivity
 import com.msabhi.composedrive.declarative.DeclarativeListActivity
+import com.msabhi.composedrive.declarative.StarterComposeActivity
 import com.msabhi.composedrive.imperative.ImperativeCounterActivity
 import com.msabhi.composedrive.imperative.ImperativeListActivity
 import com.msabhi.composedrive.mixed.MixedCounterActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private val buttonOpenStarterCompose by lazy { findViewById<Button>(R.id.button_open_starter_compose) }
 
     private val buttonOpenImperativeCounter by lazy { findViewById<Button>(R.id.button_open_imperative_counter) }
     private val buttonOpenDeclarativeCounter by lazy { findViewById<Button>(R.id.button_open_declarative_counter) }
@@ -26,6 +29,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        buttonOpenStarterCompose.setOnClickListener {
+            startActivity(
+                Intent(
+                    this,
+                    StarterComposeActivity::class.java
+                )
+            )
+        }
         buttonOpenImperativeCounter.setOnClickListener {
             startActivity(
                 Intent(
